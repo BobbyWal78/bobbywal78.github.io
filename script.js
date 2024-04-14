c0b5db0ff948baf4ac3867d549106fcd854d7d97
Bobby Walters <40669542@live.napier.ac.uk>
2024-04-14 16:18:47 -0400

    add code to clear email field

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 function subscribe() {
     // Get the email address entered by the user
     var email = document.getElementById('email').value;
-
-    // Check if the email is not empty
+    var email = emailInput.value;
+        // Check if the email is not empty
     if (email.trim() !== '') {
         // Display a popup message
         alert('Thank You For Subscribing!');
+      // Clear the email input field
+      emailInput.value = '';  
     } else {
         // If email is empty, prompt the user to enter an email address
         alert('Please enter your email address.');
