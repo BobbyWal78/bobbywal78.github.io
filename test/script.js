1be69729f467b3331a073edfcec4f18d4691c7eb
BobbyWal78 <40669542@live.napier.ac.uk>
2024-04-22 22:32:05 -0400

    CLEAN UP UNCESARY STYLING CODE, ADD BACKGORUN IMAGE

--- test/script.js
+++ test/script.js
@@ -1,3 +1,57 @@
+// follow Me Subscrobe function //
+
+function subscribe() {
+    // Get the email address entered by the user
+    var email = document.getElementById('email').value;
+   
+        // Check if the email is not empty
+    if (email.trim() !== '') {
+        // Display a popup message
+        alert('Thank You For Subscribing!');
+     
+    } else {
+        // If email is empty, prompt the user to enter an email address
+        alert('Please enter your email address.');
+    }
+}
+
+// end of follow me subscribe function //
+
+
+//  pop-up message when the contact form.htmlhas been completed
+
+document.addEventListener('DOMContentLoaded', function () {
+    var contactForm = document.getElementById('contactForm');
+    var submitBtn = document.getElementById('submitBtn');
+    var messageSent = document.getElementById('messageSent');
+
+    submitBtn.addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent form submission
+        
+        // Get form data
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var subject = document.getElementById('subject').value;
+        var message = document.getElementById('message').value;
+        
+                
+        // Display a popup message
+        messageSent.style.display = 'block'; // Show the message
+        setTimeout(function () {
+            messageSent.style.display = 'none'; // Hide the message after 3 seconds
+        }, 3000);
+
+        // Clear the form fields
+        contactForm.reset();
+    });
+});
+
+// end of pop up message contact form.html
+
+
+
+
+// expand view container in services
 const images = document.querySelectorAll('.image-container img');
 
     images.forEach(img => {
@@ -20,3 +74,20 @@ const images = document.querySelectorAll('.image-container img');
             document.body.appendChild(expandedView);
         });
     });
+
+// END expand view container in services
+
+
+
+// Disable Right Click on Images //
+window.onload = function() {
+    var images = document.getElementsByTagName('img');
+    for (var i = 0; i < images.length; i++) {
+        images[i].oncontextmenu = function(e) {
+            e.preventDefault();
+            alert("Images cannot be saved.");
+        };
+    }
+};
+
+// End of Disable Right Click on Images //
